21d6f0f733ccdcf5625bca18f87af3a5fc667735
Krenar123 <krenardardhishta205@gmail.com>
2020-08-12 19:06:34 +0200

    Actions for creating updating and deleting instruction

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'recipes#index'
 
   resources :recipes do
-    resources :ingridients, except: [:index, :show]
+    resources :ingridients, :instructions, except: [:index, :show]
   end
 end
