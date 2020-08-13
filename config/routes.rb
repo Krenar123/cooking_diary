3c107271bc4d0f3ac38a46349b91f5f65091244d
Krenar123 <krenardardhishta205@gmail.com>
2020-08-14 00:34:47 +0200

    Model user with password digest created

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :ingridients, :instructions, except: [:index, :show]
   end
+
+  get '/signup', to: 'users#new', as: :new_user
+  resources :users, except: [:index, :new, :destroy]
 end
