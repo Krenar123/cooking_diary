ad6f3e9c71830290b8edb4eab49fdbee022552d2
Krenar123 <krenardardhishta205@gmail.com>
2020-08-14 14:29:11 +0200

    Migration for adding user to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_222456) do
+ActiveRecord::Schema.define(version: 2020_08_14_122729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_08_13_222456) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema.define(version: 2020_08_13_222456) do
   end
 
   add_foreign_key "ingridients", "recipes"
+  add_foreign_key "recipes", "users"
 end
